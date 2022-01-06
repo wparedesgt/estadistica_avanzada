a2618f740b8a81f107ce8105994a53bbfb4f089f
wparedesgt <wparedesgt@gmail.com>
2022-01-06 10:47:30 -0600

    Terminando la leccion

--- analisis/04_within_within.R
+++ analisis/04_within_within.R
@@ -13,17 +13,35 @@
 
 
 library(tidyverse)
+library(car)
 
-diet <- read_csv("data/diet2.csv")
+fact <- read.csv("data/factors-within-within.csv")
+diet <- read.csv("data/diet2.csv")
 
-fact <- read_csv("data/factors-within-within.csv")
+fact$Exercise <- as.factor(fact$Exercise)
+fact$Time <- as.factor(fact$Time)
+
+
+
+str(fact)
 
 
 ###Crear una matriz con todas las variables dependientes
-
 
 weight_01 <- cbind(diet$weight_beg, diet$weight_mid, diet$weight_end, 
                    diet$weight_beg_ex, diet$weight_mid_ex, diet$weight_end_ex)
 
 #Obteniendo los promedios de las variables dependientes
 
+model <- lm(weight_01~1)
+summary(model)
+
+#Corriendo el ANOVA
+
+
+model_02 <- Anova(model, idata = fact, idesign =~Exercise*Time, type = "III")
+
+summary(model_02, multivariate = FALSE) # Le pusimos falso porque no necesitamos los resultados MANOVA
+
+
+#
